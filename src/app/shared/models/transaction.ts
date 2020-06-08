921defe62479a5cab1500e37a7c2ce8224878f81
Gabriel Loiácono <gabriel.vloiacono@gmail.com>
2020-06-08 10:54:16 -0300

    feat(models): added shared models

---
+++ src/app/shared/models/transaction.ts
@@ -0,0 +1,16 @@
+export interface TransactionPayload {
+  // Card Info
+  card_number: string;
+  cvv: number;
+  expiry_date: string;
+
+  // Destination User ID
+  destination_user_id: number;
+
+  // Value of the Transaction
+  value: number;
+}
+
+export class Transaction {
+  constructor(transaction: TransactionPayload) {}
+}
