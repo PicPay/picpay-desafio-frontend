0cb76377b4ff8e9b7e9f873c2a78c65db821234e
Gabriel Loiácono <gabriel.vloiacono@gmail.com>
2020-06-08 10:52:38 -0300

    feat(e2e): added e2e spec

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,40 @@
-import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { AppPage } from "./app.po";
+import { browser, logging } from "protractor";
 
-describe('workspace-project App', () => {
+describe("workspace-project App", () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it("should display header", () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('picpay-desafio-frontend app is running!');
+    expect(page.getPageTitle()).toEqual("SendPay");
   });
+
+  it('should have users card', () => {
+    browser.waitForAngular()
+
+    expect(page.getUserCardButtonText()).toEqual("Pagar")
+  })
+
+  it('should open modal', async () => {
+    browser.waitForAngular()
+
+    const el = await page.getUserCardButton()
+    el.click()
+
+    expect(page.getModalHeaderText()).toEqual("Pagamento")
+  })
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+    expect(logs).not.toContain(
+      jasmine.objectContaining({
+        level: logging.Level.SEVERE,
+      } as logging.Entry)
+    );
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,29 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, WebElementPromise } from "protractor";
 
 export class AppPage {
   navigateTo() {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
-  getTitleText() {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getPageTitle() {
+    return element(by.css("app-header p.text-gray-800")).getText() as Promise<
+      string
+    >;
+  }
+
+  getUserCardButtonText() {
+    return element(
+      by.css("app-user-item app-button button")
+    ).getText() as Promise<string>;
+  }
+
+  getUserCardButton() {
+    return element(
+      by.css("app-user-item app-button button")
+    ).getWebElement() as WebElementPromise;
+  }
+
+  getModalHeaderText() {
+    return element(by.css("app-modal header h2")).getText() as Promise<string>;
   }
 }
