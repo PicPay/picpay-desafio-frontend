168c32e8f760bc9466e0a5765d9d3a1e377dba9a
cabukater <cabukater@gmail.com>
2020-06-27 22:43:26 -0300

    insert service users

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,35 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  url = 'https://www.mocky.io/v2/5d531c4f2e0000620081ddce'; 
+  constructor(private httpClient: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>(this.url)
+      .pipe(
+        retry(2),
+        catchError(this.handleError))
+  }
+
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Erro ocorreu no lado do client
+      errorMessage = error.error.message;
+    } else {
+      // Erro ocorreu no lado do servidor
+      errorMessage = `Código do erro: ${error.status}, ` + `menssagem: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  };
+
+}
