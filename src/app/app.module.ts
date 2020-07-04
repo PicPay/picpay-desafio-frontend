3c2529571b74dab05455436c3609d98b1372a947
Larissa Soares <lari_soares9@hotmail.com>
2020-07-04 11:29:05 -0300

    Configure the initial project structure - Create routing file - Create main components: Header, Users List and Modal - Change initial styles of users list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ModalComponent, UsersListComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
