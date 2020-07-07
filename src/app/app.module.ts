905ef177e518d6f230d0c975b4dd81ca3efada5e
Hardy Miranda <hardymiranda@brq.com>
2020-07-07 00:20:49 -0300

    :wrench: Add Forms, Modals, ApiService and ModalComponent to Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbActiveModal, NgbModal, NgbModule, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CurrencyMaskModule } from 'ng2-currency-mask';
+
+import { ApiService } from './services/api.service';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +14,21 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CurrencyMaskModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    ApiService,
+    NgbActiveModal
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [
+    ModalComponent
+  ]
 })
+
 export class AppModule { }
