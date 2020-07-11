081481dfdd33a9c42e728cc507d9703b8e175a69
Leticia Mazzo <leticiamazzo@gmail.com>
2020-07-11 14:45:36 -0300

    feat: consume user data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Desafio Picpay Front-end';
+ 
+  cards = [
+    // valid card
+    {
+      card_number: '1111111111111111',
+      cvv: 789,
+      expiry_date: '01/18',
+    },
+    // invalid card
+    {
+      card_number: '4111111111111234',
+      cvv: 123,
+      expiry_date: '01/20',
+    },
+  ];
+
+  users: Object[] = [];
+
+  constructor(http: HttpClient) {
+
+    http
+    .get<Object[]>('https://www.mocky.io/v2/5d531c4f2e0000620081ddce')
+    .subscribe(users => this.users = users)
+
+    // http
+    //   .get<Object[]>('https://www.mocky.io/v2/5d531c4f2e0000620081ddce')
+    //   .subscribe(users => {
+    //     console.log(users);
+    //     this.users = users
+    //   })
+  }
 }
