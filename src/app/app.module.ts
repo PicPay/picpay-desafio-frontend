ed47ba23e4c6ad0e386d348881dce7ed8dd4c374
rafael <rafael.silva@itixti.com.br>
2020-07-11 17:42:18 -0300

    adiciona imports e modifica core angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { PayModalComponent } from './components/pay-modal/pay-modal.component';
+import { PayUsersComponent } from './components/pay-users/pay-users.component';
+import { PayUserComponent } from './components/pay-users/pay-user/pay-user.component';
+import { PayMessageComponent } from './components/pay-message/pay-message.component';
+import { PayUserService } from './services/pay-users.service';
+import { ModalModule  } from 'ngx-bootstrap/modal';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PayModalComponent,
+    PayUsersComponent,
+    PayUserComponent,
+    PayMessageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ModalModule.forRoot(),
+    ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    PayUserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
