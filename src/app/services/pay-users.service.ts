8bf99cc00d4c94991ee18c7ba8a7df0f4951c03a
rafael <rafael.silva@itixti.com.br>
2020-07-12 18:17:52 -0300

    refactory service payload

--- src/app/services/pay-users.service.ts
+++ src/app/services/pay-users.service.ts
@@ -1,11 +1,10 @@
-// import { Restaurant } from './restaurant/restaurant.model';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { PayUser } from 'src/app/components/pay-users/pay-user/pay-user.model';
 import { TransactionPayLoad } from 'src/app/models/TransactionPayLoad.model';
-
+import {environment } from 'src/environments/environment';
 @Injectable()
 export class PayUserService {
 
@@ -13,12 +12,12 @@ export class PayUserService {
 
     getUsers(): Observable<PayUser[]> {
         return this.http
-            .get<PayUser[]>(`https://private-ba7f3-rprteste.apiary-mock.com/users`)
-            .pipe(map(response => response))
+            .get<PayUser[]>(`${environment.apiUsers}`)
+            .pipe(map(response => response));
     }
 
     postSendPayment(transactionPayLoad: TransactionPayLoad): Observable<TransactionPayLoad> {
         return this.http
-            .post<TransactionPayLoad>('https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989', transactionPayLoad);
+            .post<TransactionPayLoad>(`${environment.apiTransaction}`, transactionPayLoad);
     }
 }
