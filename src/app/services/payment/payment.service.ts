caec6f6e50ca0a47edb4e6f23abb5240a6297d1b
Marco Lima <marco.lima@itixti.com.br>
2020-07-18 13:51:12 -0300

    refactoring urls with environments

--- src/app/services/payment/payment.service.ts
+++ src/app/services/payment/payment.service.ts
@@ -4,11 +4,10 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CreditCard } from 'src/app/models/credit-card.model';
 import { PaymentReturn } from 'src/app/models/payment-return.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class PaymentService {
-
-    private url = 'https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989';
 
     constructor(
         private httpClient: HttpClient
@@ -20,7 +19,7 @@ export class PaymentService {
      * @return Um Observable contendo a efetivação do pagamento
      */
     processPayment(payment: Payment): Observable<PaymentReturn> {
-        return this.httpClient.post<PaymentReturn>(this.url, payment);
+        return this.httpClient.post<PaymentReturn>(environment.paymentURL, payment);
     }
 
     /** Valida se um cartão é válido para pagamento ou não */
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -2,19 +2,18 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { User } from 'src/app/models/user.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  private url = 'https://www.mocky.io/v2/5d531c4f2e0000620081ddce';
-
   constructor(
     private httpClient: HttpClient
   ) { }
 
   getUsers(): Observable<User[]> {
-    return this.httpClient.get<User[]>(this.url);
+    return this.httpClient.get<User[]>(environment.usersURL);
   }
 }
