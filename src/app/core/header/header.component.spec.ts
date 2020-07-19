a621ec638f1a0ee151596ab46f9913100ac3647d
Bernardo Simonassi Moura <bernardosimonassi@hotmail.com>
2020-07-19 16:36:50 -0300

    test: mudei a descrição do teste

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -19,7 +19,7 @@ describe('HeaderComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('deve criar', () => {
     expect(component).toBeTruthy();
   });
 });
