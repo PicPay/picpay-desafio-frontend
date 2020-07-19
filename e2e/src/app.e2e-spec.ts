2d902e510ea9d8bbc20942753c9099698e8d3fa4
Bernardo Simonassi Moura <bernardosimonassi@hotmail.com>
2020-07-19 19:50:49 -0300

    test: finalizei os testes e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,49 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('picpay-desafio-frontend app is running!');
+  it('deve navegar para a pagina inicial', () => {
+    page.navigateToStart();
+    expect(page.getTitle()).toEqual('Usuários');
+  });
+
+  it('deve selecionar um usuário e ir para a página de pagamento', () => {
+    page.clearLocalStorage();
+    page.selectUserButton.click();
+    expect(page.getPath()).toContain('pagamento');
+  });
+
+  it('deve ir para a página de cadastrar cartão', () => {
+    page.goToNewCardPageButton.click();
+    expect(page.getPath()).toContain('novo-cartao');
+  });
+
+  it('deve preencher formulário de cadastrar cartão', () => {
+    page.cardNumberField.sendKeys('1111111111111111');
+    page.cvvField.sendKeys('789');
+    page.expiryDateField.sendKeys('0118');
+    expect(page.cardNumberField.getAttribute('value')).toEqual('1111 1111 1111 1111');
+    expect(page.cvvField.getAttribute('value')).toEqual('789');
+    expect(page.expiryDateField.getAttribute('value')).toEqual('01/18');
+  });
+
+  it('deve clicar no botão de criar cartão e ir para a página de meus cartões', () => {
+    page.createCardButton.click();
+    expect(page.getPath()).toContain('cartoes');
+  });
+
+  it('deve selecionar o primeiro cartão e ir para a página de pagamento', () => {
+    page.selectCardButton.click();
+    expect(page.getPath()).toContain('pagamento');
+  });
+
+  it('deve preencher o campo de valor', () => {
+    page.valueField.sendKeys('2000');
+    expect(page.valueField.getAttribute('value')).toEqual('R$ 20,00');
+  });
+
+  it('deve enviar pagamento e ir para a página inicial', () => {
+    page.sendPaymentButton.click();
+    expect(page.getTitle()).toEqual('Usuários');
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,33 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+
+  selectUserButton = element(by.css('.pay'));
+  goToNewCardPageButton = element(by.css('.payment-form'));
+  createCardButton = element(by.css('.btn-primary'));
+  selectCardButton = element(by.css('.select'));
+  sendPaymentButton = element(by.id('send-payment'));
+
+  valueField = element(by.name('value'));
+
+  cardNumberField = element(by.name('card_number'));
+  cvvField = element(by.name('cvv'));
+  expiryDateField = element(by.name('expiry_date'));
+
+  navigateToStart = () => {
+    return browser.get('/');
   }
 
-  getTitleText() {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getTitle = () => {
+    return element(by.css('.head .title')).getText();
   }
+
+  clearLocalStorage = () => {
+    browser.executeScript('window.localStorage.clear();');
+  }
+
+  getPath = () => {
+    return browser.getCurrentUrl();
+  }
+
 }
