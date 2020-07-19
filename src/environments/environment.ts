49522541b71df683352c960399d682de930a6512
Lucas Simões <lucasousi@gmail.com>
2020-07-19 01:23:14 -0300

    Adicionado variável para identificar build de produção no environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  production: false,
   apiUsersURL: 'https://www.mocky.io/v2',
   apiTransactionUrl: 'https://run.mocky.io/v3', // 533cd5d7-63d3-4488-bf8d-4bb8c751c989
 };
