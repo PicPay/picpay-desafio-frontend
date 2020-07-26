3e5b45afd725f8b10ad5d14167b90f64be266211
Rafael Pelicioni <rafaelpelicione@gmail.com>
2020-07-26 20:03:38 -0300

    Alter app module and index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/users/user/user.component';
+import { PaymentComponent } from './components/payment/payment.component';
+import { UserService } from './services/user/user.service';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbActiveModal, NgbModalModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CurrencyPipe } from '@angular/common';
+import { ReceiptComponent } from './components/receipt/receipt.component';
+import { NgxCurrencyModule } from "ngx-currency";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    PaymentComponent,
+    ReceiptComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgbModalModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxCurrencyModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    UserService,
+    NgbActiveModal,
+    [CurrencyPipe, {
+      provide: LOCALE_ID,
+      useValue: "en-US"
+    }]
+  ],
+  bootstrap: [
+    AppComponent
+  ],
+  entryComponents: [
+    PaymentComponent,
+    ReceiptComponent
+  ]
 })
 export class AppModule { }
