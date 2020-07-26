8716756fe7c7fd9ad330dabf65becde80ba9ef60
Rafael Pelicioni <rafaelpelicione@gmail.com>
2020-07-26 20:02:57 -0300

    Add transaction service and variables

--- src/app/services/transaction/transaction.service.ts
+++ src/app/services/transaction/transaction.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Transaction } from 'src/app/models/transaction.model';
+import { Observable } from 'rxjs';
+import { TransactionResult } from 'src/app/models/transaction-result.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  public create(transaction: Transaction): Observable<TransactionResult> {
+    return this.http.post<TransactionResult>(environment.apiTransaction, transaction)
+  }
 }
