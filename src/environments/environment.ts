700b61b292df8e13ffefc12c91dc217d085c0868
Rafael Pelicioni <rafaelpelicione@gmail.com>
2020-07-26 20:00:22 -0300

    Alter environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  'apiUsers': 'https://private-ba7f3-rprteste.apiary-mock.com/users',
-  'apiTransaction': 'https://5f0b66a99d1e150016b374d9.mockapi.io/api/payload'
+  'apiUsers': 'https://www.mocky.io/v2/5d531c4f2e0000620081ddce',
+  'apiTransaction': 'https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989'
 };
 
 /*
