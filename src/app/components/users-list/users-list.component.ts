2a37daf24cdac90b7f6aad67dc51a377ebfc907a
bibull <rubens.andrade@lejour.com.br>
2021-01-25 17:32:50 -0300

    made user list component open the payment modal

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
-import { User } from "src/app/interfaces/user";
-import { takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
-import { UsersService } from 'src/app/services/users.service';
+import { User } from 'src/app/interfaces/user.interface';
+import { takeUntil } from "rxjs/operators";
+import { Subject } from "rxjs";
+import { UsersService } from "src/app/services/user-service/users.service";
+import { MatDialog } from "@angular/material/dialog";
+import { PaymentModalComponent } from "../modals/payment-modal/payment-modal.component";
 
 @Component({
   selector: "app-users-list",
@@ -13,8 +15,10 @@ export class UsersListComponent implements OnInit, OnDestroy {
   private readonly unsubscribe$: Subject<void> = new Subject<void>();
   public users: User[];
 
-  constructor(private UsersService: UsersService) {
-    this.UsersService.getUsers().pipe(takeUntil(this.unsubscribe$)).subscribe(users => this.users = users)
+  constructor(private UsersService: UsersService, public dialog: MatDialog) {
+    this.UsersService.getUsers()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((users) => (this.users = users));
   }
 
   ngOnInit() {}
@@ -22,5 +26,10 @@ export class UsersListComponent implements OnInit, OnDestroy {
   public ngOnDestroy(): void {
     this.unsubscribe$.next();
     this.unsubscribe$.complete();
+  }
+
+  public openPaymentModal(user: User): void {
+    const paymentModal = this.dialog.open(PaymentModalComponent);
+    paymentModal.componentInstance.user = user;
   }
 }
