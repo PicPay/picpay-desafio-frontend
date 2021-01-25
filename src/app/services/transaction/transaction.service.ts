0d5d97cbd42694c1f266f0ac9dcb66b54d218f9e
bibull <rubens.andrade@lejour.com.br>
2021-01-25 15:47:50 -0300

    added transaction service

---
+++ src/app/services/transaction/transaction.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TransactionPayload, TransactionStatus } from 'src/app/interfaces/transaction';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransactionService {
+  constructor(private http: HttpClient) {}
+
+  public payload(transaction: TransactionPayload): Observable<TransactionStatus> {
+    return this.http.post<TransactionStatus>('https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989', transaction)
+  }
+
+}
