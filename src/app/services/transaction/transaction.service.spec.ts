cf7b32f1f1bb31e14fc2c3f4bb5f53688d2c2809
bibull <rubens.andrade@lejour.com.br>
2021-01-26 04:12:38 -0300

    added transaction service unit tests

--- src/app/services/transaction/transaction.service.spec.ts
+++ src/app/services/transaction/transaction.service.spec.ts
@@ -1,12 +1,50 @@
-import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
 
-import { TransactionService } from './transaction.service';
+import { TransactionService } from "./transaction.service";
 
-describe('TransactionService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe("TransactionService", () => {
+  let transactionService: TransactionService;
+  let http: HttpTestingController;
 
-  it('should be created', () => {
-    const service: TransactionService = TestBed.get(TransactionService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [TransactionService],
+    });
+
+    transactionService = TestBed.get(TransactionService);
+    http = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    http.verify();
+  });
+
+  it("should be created", () => {
+    expect(transactionService).toBeTruthy();
+  });
+
+  it("should return success transaction", () => {
+    transactionService
+      .payload({
+        card_number: "1111111111111111",
+        cvv: 789,
+        expiry_date: "01/18",
+        destination_user_id: 1010,
+        value: 10000,
+      })
+      .subscribe((status) => {
+        expect(status).toEqual({
+          success: true,
+          status: "Aprovada",
+        });
+      });
+
+    const req = http.expectOne('https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989');
+    expect(req.request.method).toBe('POST');
   });
 });
--- src/app/services/transaction/transaction.service.ts
+++ src/app/services/transaction/transaction.service.ts
@@ -1,17 +1,23 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { TransactionPayload, TransactionStatus } from 'src/app/interfaces/transaction.interface';
-
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import {
+  TransactionPayload,
+  TransactionStatus,
+} from "src/app/interfaces/transaction.interface";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class TransactionService {
   constructor(private http: HttpClient) {}
 
-  public payload(transaction: TransactionPayload): Observable<TransactionStatus> {
-    return this.http.post<TransactionStatus>('https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989', transaction)
+  public payload(
+    transaction: TransactionPayload
+  ): Observable<TransactionStatus> {
+    return this.http.post<TransactionStatus>(
+      "https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989",
+      transaction
+    );
   }
-
 }
