1dd285fffa451c1772fa10baede829bab7f2ddf9
samych4n <samuel@firmamento.com.br>
2021-01-29 14:09:59 -0300

    feat(core): Coloca lógica na Home
    Home agora pegando dados da api e colocando na página

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ApiService } from 'src/app/services/api/api.service';
-import { IApi } from 'src/app/services/api/interfaces/api.interface';
+import { IApi } from 'src/app/interfaces/api.interface';
+import { Observable, Subscription } from 'rxjs';
+import { IUser } from 'src/app/interfaces/user.interface';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +10,39 @@ import { IApi } from 'src/app/services/api/interfaces/api.interface';
   styleUrls: ['./home.component.scss'],
   providers: [{ provide: IApi, useClass: ApiService },]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  usersData = {
+    isLoading:true,
+    error:null,
+    data:null
+  }
+  selectedUser:IUser;
+  getUsersSubscription:Subscription;
+  pagamentoComSucesso:boolean = null;
 
   constructor(private api:IApi) { }
 
   ngOnInit() {
-    this.api.getUsers().subscribe((val) => console.log(val))
+    this.refreshUsers();
+  }
+
+  refreshUsers(){
+    this.usersData = {
+      isLoading:true,
+      error:null,
+      data:null
+    }
+    this.getUsersSubscription = this.api.getUsers().subscribe(
+      (data) => {this.usersData.isLoading = false;this.usersData.data = data},
+      (error) => {this.usersData.isLoading = false;this.usersData.error = error},);
+  }
+
+  ngOnDestroy(): void {
+    if(this.getUsersSubscription){this.getUsersSubscription.unsubscribe;}
+  }
+  
+  OnPagar(user:IUser){
+    this.selectedUser=user;
   }
 
 }
