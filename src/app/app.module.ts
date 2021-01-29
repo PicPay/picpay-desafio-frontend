d423f558f6d4b37735c66f0d8d375caa16e3f69d
samych4n <samuel@firmamento.com.br>
2021-01-29 08:04:41 -0300

    refactor(core): componentiza elementos da home
    Estavam todos os elementos na pagina de entrada, logo foi necessario separa-los para começar a trabalhar com eles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AvatarComponent } from './components/avatar/avatar.component';
+import { UserRowComponent } from './components/user-row/user-row.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AvatarComponent,
+    UserRowComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule
