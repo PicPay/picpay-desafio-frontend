2766b45adece2926a1e4147264c6cdc1add42acf
Calazans <calazansdev@gmail.com>
2021-01-29 02:59:26 -0300

    feat: adjustment to open transaction model

---
+++ src/app/model/card.ts
@@ -0,0 +1,5 @@
+export default interface Card {
+  card_number: string;
+  cvv: number;
+  expiry_date: string;
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '@service/user.service';
 import { User } from '@model/user';
 import { dynamicSort } from 'src/app/utils/sort';
+import { MatDialog } from '@angular/material/dialog';
+import { TransactionComponent } from './transaction/transaction.component';
 
 @Component({
   selector: 'app-home',
@@ -10,9 +12,13 @@ import { dynamicSort } from 'src/app/utils/sort';
 })
 export class HomeComponent implements OnInit {
 
+  loading: boolean;
+
+  private LIST_ORIENTATION = 'name'
+
   public users: User[]
 
-  constructor(private userService: UserService){
+  constructor(private userService: UserService, public dialog: MatDialog){
   }
 
   ngOnInit() {
@@ -20,10 +26,24 @@ export class HomeComponent implements OnInit {
   }
 
   getAllUser(){
+    this.loading = true;
     this.userService.getAll().subscribe(response =>{
-      response.sort(dynamicSort('name'))
+      response.sort(dynamicSort(this.LIST_ORIENTATION))
       this.users = response
+      this.loading = false;
+    },()=>{
+      this.loading = false;
     })
+  }
+
+  openPay(user: User){
+    const dialogRef = this.dialog.open(TransactionComponent, {
+      width: '350px',
+      
+      data: { user }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {});
   }
 
 }
