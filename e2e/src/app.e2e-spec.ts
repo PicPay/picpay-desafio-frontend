7b738543efc3421c447d954ab92f97b844e9168b
Thiago Leite <thiagofmleite@gmail.com>
2021-02-02 17:24:05 -0300

    Testes E2E

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,42 @@
-import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { AppPage } from "./app.po";
+import { browser, element, logging } from "protractor";
 
-describe('workspace-project App', () => {
+describe("workspace-project App", () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it("should display a list of users", () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('picpay-desafio-frontend app is running!');
+    expect(page.getUsers().isPresent()).toBeTruthy();
+  });
+
+  it("should display a payment modal when click on user button", () => {
+    page.navigateTo();
+    page.clickPayButton();
+    expect(page.getPaymentModal().isPresent()).toBeTruthy();
+  });
+
+  it("should create a transaction", () => {
+    page.navigateTo();
+    page.clickPayButton();
+    browser.wait(page.getPaymentModal().isPresent());
+    page.getValueInput().sendKeys(10);
+    page.getCardsSelect().click();
+    page.getOptions().get(0).click();
+    page.getModalPayButton().click();
+    expect(page.getModalTitleText()).toEqual("Recibo de pagamento");
   });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+    expect(logs).not.toContain(
+      jasmine.objectContaining({
+        level: logging.Level.SEVERE,
+      } as logging.Entry)
+    );
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,4 +1,4 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element } from "protractor";
 
 export class AppPage {
   navigateTo() {
@@ -6,6 +6,46 @@ export class AppPage {
   }
 
   getTitleText() {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return element(
+      by.css("app-root .content span")
+    ).getText() as Promise<string>;
+  }
+
+  getUsers() {
+    return element(by.css("app-root .users"));
+  }
+
+  clickPayButton() {
+    return element
+      .all(by.css("app-root .users .user"))
+      .get(0)
+      .element(by.css(".pay-button"))
+      .click();
+  }
+
+  getPaymentModal() {
+    return element(by.css("app-root .modal"));
+  }
+
+  getValueInput() {
+    return element(by.css("app-root .modal input[type=number]"));
+  }
+
+  getCardsSelect() {
+    return element(by.css("app-root .modal .cards select"));
+  }
+
+  getOptions() {
+    return element.all(by.css("app-root .modal .cards select option"));
+  }
+
+  getModalPayButton() {
+    return element(by.css("app-root .modal .pay-button"));
+  }
+
+  getModalTitleText() {
+    return element(
+      by.css("app-root .modal .title")
+    ).getText() as Promise<string>;
   }
 }
