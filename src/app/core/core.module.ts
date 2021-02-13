f0e0195610bf563bdb6c06246ccb7c7a669754d7
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-13 16:32:41 -0300

    chore(core): import theme service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,10 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ApiService } from '@core/services/api/api.service';
+import { ThemeService } from './services/theme/theme.service';
 
 @NgModule({
   imports: [HttpClientModule],
-  providers: [HttpClientModule, ApiService],
+  providers: [HttpClientModule, ApiService, ThemeService],
 })
 export class CoreModule {}
