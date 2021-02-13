58e7076cad9abb524bedeaf4c1ba63175bd3ffb4
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-13 19:53:00 -0300

    refactor(shared): remove form validator service

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,12 @@
+import { CoreModule } from '@core/core.module';
 import { NgModule } from '@angular/core';
 import { ComponentsModule } from '@shared/components/components.module';
-import { FormControlValidatorService } from '@shared/services/form-control-validator/form-control-validator.service';
 import { TransactionService } from '@shared/services/transaction/transaction.service';
 import { UserService } from '@shared/services/user/user.service';
 
 @NgModule({
-  exports: [
-    ComponentsModule
-  ],
-  providers: [
-    TransactionService,
-    UserService,
-    FormControlValidatorService
-  ]
+  imports: [CoreModule],
+  exports: [ComponentsModule],
+  providers: [TransactionService, UserService],
 })
 export class SharedModule {}
