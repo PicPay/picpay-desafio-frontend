ffa93c6ecf20fe94146735f3e11f0363c1663241
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-13 11:59:00 -0300

    test: add api.service unit test, test get method

---
+++ src/app/core/services/api/api.service.spec.ts
@@ -0,0 +1,37 @@
+import { CoreModule } from './../../core.module';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { ApiService } from '@core/services/api/api.service';
+
+describe('ApiService', () => {
+  let apiService: ApiService;
+  let httpTestingController: HttpTestingController;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ApiService],
+    });
+
+    apiService = TestBed.get(ApiService);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+  it('should send http get method', () => {
+    apiService
+      .list<[{ id: number }]>('someUrl')
+      .subscribe((result) => expect(result).toEqual([{ id: 1 }]));
+
+    const request = httpTestingController.expectOne('someUrl');
+
+    expect(request.request.method).toEqual('GET');
+
+    request.flush([{ id: 1 }]);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+});
