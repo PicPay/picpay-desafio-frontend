c5a61131c7e9be45b1a714a0e03c3065a2060dc4
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-13 19:19:39 -0300

    refactor(transaction-form): add mock card on transaction form card mock

--- src/app/shared/mocks/transaction/transaction-form.mock.ts
+++ src/app/shared/mocks/transaction/transaction-form.mock.ts
@@ -1,17 +1,11 @@
+import { MOCK_INVALID_CARD } from './../card/card.mock';
 import { Card } from '@core/domains/card/card.domain';
 import { TransactionForm } from '@shared/components/transaction-form-modal/transaction-form-modal.component';
+import { MOCK_VALID_CARD } from '../card/card.mock';
 
 export const MOCK_TRANSACTION_FORM_CARDS: Card[] = [
-  {
-    card_number: '1111111111111111',
-    cvv: 789,
-    expiry_date: '01/18',
-  },
-  {
-    card_number: '4111111111111234',
-    cvv: 123,
-    expiry_date: '01/20',
-  },
+  MOCK_VALID_CARD,
+  MOCK_INVALID_CARD,
 ];
 
 export const MOCK_TRANSACTION_FORM_DATA: TransactionForm = {
