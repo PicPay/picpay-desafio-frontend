9592a397938e64b3504b495ff695f7662fb392ee
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-13 18:02:04 -0300

    feat(user-card): add user card component

---
+++ src/app/shared/components/user-card/user-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { User } from '@core/domains/user/user.domain';
+
+@Component({
+  selector: 'app-user-card',
+  templateUrl: './user-card.component.html',
+  styleUrls: ['./user-card.component.scss']
+})
+export class UserCardComponent implements OnInit {
+
+  @Input() user: User;
+
+  @Output() pay: EventEmitter<User> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onClickPay(user: User) {
+    this.pay.emit(user);
+  }
+}
