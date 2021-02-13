cea1566103e4d9b89bfe99d87e6dacaf073aac3f
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-13 12:00:24 -0300

    chore: add api.service, implements list method

---
+++ src/app/core/services/api/api.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ApiService {
+  constructor(private httpClient: HttpClient) {}
+
+  list<T>(url: string): Observable<T> {
+    return this.httpClient.get<T>(`${url}`);
+  }
+}
