6e2c567529887bea224af47f68d11ba1dfcbb1e2
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-15 15:44:20 -0300

    test(form-control-validator): edit fom control validator test adding vocabulary context

--- src/app/core/services/form-control-validator/form-control-validator.service.spec.ts
+++ src/app/core/services/form-control-validator/form-control-validator.service.spec.ts
@@ -1,8 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { AbstractControl, FormControl, Validators } from '@angular/forms';
-import { FormValidatorMessage } from '@core/enums/form-validator-message.enum';
 import { SharedModule } from '@shared/shared.module';
 import { FormControlValidatorService } from './form-control-validator.service';
+import { FORM_CONTROL_VALIDATOR_VOCABULARY } from './form-control-validator.service.vocabulary';
 
 describe('FormControlValidatorService', () => {
   let formControlValidatorService: FormControlValidatorService;
@@ -10,38 +10,39 @@ describe('FormControlValidatorService', () => {
   let minLengthControl: AbstractControl;
   let requiredControl: AbstractControl;
 
+  const fieldName = 'Valor';
+  const fieldValue = 0.9;
+  const length = 1;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [SharedModule],
     });
 
     formControlValidatorService = TestBed.get(FormControlValidatorService);
-    minLengthControl = new FormControl(0.9, [
-      Validators.min(1),
+    minLengthControl = new FormControl(fieldValue, [
+      Validators.min(length),
       Validators.required,
     ]);
     requiredControl = new FormControl('', [Validators.required]);
   });
 
   it('should retrive required message with form field name', () => {
-    const message = formControlValidatorService.getRequiredMessage('Valor');
+    const message = formControlValidatorService.getRequiredMessage(fieldName);
     expect(message).toBeTruthy();
-    expect(message).toEqual(
-      FormValidatorMessage.REQUIRED.replace('[FIELD]', 'Valor')
+    expect(message).toContain(
+      `${fieldName} ${FORM_CONTROL_VALIDATOR_VOCABULARY.required}`
     );
   });
 
   it('should retrive min length message with form field name', () => {
     const message = formControlValidatorService.getMinLengthErrorMessage(
-      1,
-      'Valor'
+      length,
+      fieldName
     );
     expect(message).toBeTruthy();
-    expect(message).toEqual(
-      FormValidatorMessage.MINLENGTH.replace('[FIELD]', 'Valor').replace(
-        '[LENGTH]',
-        '1'
-      )
+    expect(message).toContain(
+      `${fieldName} ${FORM_CONTROL_VALIDATOR_VOCABULARY.minLength}`
     );
   });
 
@@ -51,15 +52,12 @@ describe('FormControlValidatorService', () => {
 
     const message = formControlValidatorService.findError(
       minLengthControl,
-      'Valor'
+      fieldName
     );
 
     expect(message).toBeTruthy();
-    expect(message).toEqual(
-      FormValidatorMessage.MINLENGTH.replace('[FIELD]', 'Valor').replace(
-        '[LENGTH]',
-        '1'
-      )
+    expect(message).toContain(
+      `${fieldName} ${FORM_CONTROL_VALIDATOR_VOCABULARY.minLength}`
     );
   });
 
@@ -69,12 +67,12 @@ describe('FormControlValidatorService', () => {
 
     const message = formControlValidatorService.findError(
       requiredControl,
-      'Valor'
+      fieldName
     );
 
     expect(message).toBeTruthy();
-    expect(message).toEqual(
-      FormValidatorMessage.REQUIRED.replace('[FIELD]', 'Valor')
+    expect(message).toContain(
+      `${fieldName} ${FORM_CONTROL_VALIDATOR_VOCABULARY.required}`
     );
   });
 });
