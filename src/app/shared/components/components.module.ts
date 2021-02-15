06982dcf2bea3a7c291c6deeb8304ddae69018e4
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-15 16:16:22 -0300

    chore(components-module): import translate switcher component

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -14,13 +14,19 @@ import {
 } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from '@core/core.module';
 import { REAL_CURRENCY_MASK_CONFIG } from '@shared/constants/currency-config.const';
 import { NgxCurrencyModule } from 'ngx-currency';
 import { TransactionFormModalComponent } from './transaction-form-modal/transaction-form-modal.component';
+import { TranslateSwitcherComponent } from './translate-switcher/translate-switcher.component';
 import { UserCardComponent } from './user-card/user-card.component';
 
 @NgModule({
-  declarations: [UserCardComponent, TransactionFormModalComponent],
+  declarations: [
+    UserCardComponent,
+    TransactionFormModalComponent,
+    TranslateSwitcherComponent,
+  ],
   entryComponents: [TransactionFormModalComponent],
   imports: [
     BrowserModule,
@@ -38,9 +44,12 @@ import { UserCardComponent } from './user-card/user-card.component';
     MatTooltipModule,
     MatDialogModule,
     FlexLayoutModule,
+    CoreModule,
   ],
   exports: [
     UserCardComponent,
+    TranslateSwitcherComponent,
+    CoreModule,
     BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
