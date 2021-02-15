54a95f397454d62cfbb5edbfdaef6ae746b32bb8
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-15 16:23:25 -0300

    test(transaction): edit transaction service test adding translate service con configtestmodule

--- src/app/shared/services/transaction/transaction.service.spec.ts
+++ src/app/shared/services/transaction/transaction.service.spec.ts
@@ -1,19 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 import { ApiService } from '@core/services/api/api.service';
-import { MOCK_TRANSACTION_API_RESULT, MOCK_TRANSACTION_PAYLOAD } from '@shared/mocks/transaction/transaction.mock';
-import { of } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
 import {
-  TransactionService
-} from './transaction.service';
+  MOCK_TRANSACTION_API_RESULT,
+  MOCK_TRANSACTION_PAYLOAD,
+} from '@shared/mocks/transaction/transaction.mock';
+import { of } from 'rxjs';
+import { TransactionService } from './transaction.service';
 
 describe('TransactionService', () => {
   let apiServiceSpy: jasmine.SpyObj<ApiService>;
   let transactionService: TransactionService;
 
+  let translateServiceSpy: jasmine.SpyObj<TranslateService>;
+
   beforeEach(() => {
     apiServiceSpy = jasmine.createSpyObj('ApiService', ['post']);
+    translateServiceSpy = jasmine.createSpyObj('TranslateService', ['instant']);
     TestBed.configureTestingModule({
       providers: [
+        { provide: TranslateService, useValue: translateServiceSpy },
         { provide: ApiService, useValue: apiServiceSpy },
         TransactionService,
       ],
@@ -23,10 +29,14 @@ describe('TransactionService', () => {
 
   it('should send post transaction', () => {
     apiServiceSpy.post.and.returnValue(of(MOCK_TRANSACTION_API_RESULT));
-    transactionService.postTransaction(MOCK_TRANSACTION_PAYLOAD).subscribe((transaction) => {
-      expect(transaction).toBeTruthy();
-      expect(typeof transaction).toBe('object');
-      expect(transaction.destination_user.id).toEqual(MOCK_TRANSACTION_PAYLOAD.destination_user_id);
-    });
+    transactionService
+      .postTransaction(MOCK_TRANSACTION_PAYLOAD)
+      .subscribe((transaction) => {
+        expect(transaction).toBeTruthy();
+        expect(typeof transaction).toBe('object');
+        expect(transaction.destination_user.id).toEqual(
+          MOCK_TRANSACTION_PAYLOAD.destination_user_id
+        );
+      });
   });
 });
