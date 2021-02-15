de00935c85e098f166faeacffc6c6703d32d0be9
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-15 15:55:25 -0300

    feat(transaction-form-modal): add transaction form vocabulary

---
+++ src/app/shared/components/transaction-form-modal/transaction-form-modal.component.vocabulary.ts
@@ -0,0 +1,9 @@
+export const TRANSACTION_FORM_VOCABULARY = {
+  title: 'shared.components.transaction-form-modal.title',
+  value: 'shared.components.transaction-form-modal.value',
+  clearValue: 'shared.components.transaction-form-modal.clearValue',
+  card: 'shared.components.transaction-form-modal.card',
+  pay: 'shared.components.transaction-form-modal.pay',
+  cancel: 'shared.components.transaction-form-modal.cancel',
+  selectLabel: 'shared.components.transaction-form-modal.selectLabel',
+};
