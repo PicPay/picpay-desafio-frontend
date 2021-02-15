f9bda4c902b53aec31ece6b9a039461b4c0e06f2
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-15 16:12:05 -0300

    feat(user-card): add user card vocabulary

---
+++ src/app/shared/components/user-card/user-card.component.vocabulary.ts
@@ -0,0 +1,7 @@
+export const USER_CARD_VOCABULARY = {
+  name: 'shared.components.user-card.name',
+  username: 'shared.components.user-card.username',
+  id: 'shared.components.user-card.id',
+  pay: 'shared.components.user-card.pay',
+  paid: 'shared.components.user-card.paid',
+};
