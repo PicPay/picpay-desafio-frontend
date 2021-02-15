d39662c90d0d589eb7787950214da273aa3d7a23
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-14 21:52:36 -0300

    temp(env): add comment on env dev file

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,9 +2,8 @@ export const environment = {
   production: false,
   baseUsersApiUrl: 'https://www.mocky.io/v2/',
 
-  /* ESTÁ RETORNANDO CORS ERROR
-   * baseTransactionApiUrl: 'https://run.mocky.io/v3/'
-   */
+  /* ESTÁ RETORNANDO CORS ERROR */
+  // baseTransactionApiUrl: 'https://run.mocky.io/v3/'
 
   /* RETORNA SOMENTE O USER { id: 1002  name: "Marina Coelho" } independente do ID passado*/
   baseTransactionApiUrl: 'https://www.mocky.io/v2/',
