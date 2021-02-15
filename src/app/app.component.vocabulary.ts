762cd732ca6c1d5f5c1af705225aa1a930e232f4
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-15 16:46:43 -0300

    feat(app): add app vocabulary file

---
+++ src/app/app.component.vocabulary.ts
@@ -0,0 +1,16 @@
+export const APP_VOCABULARY = {
+  header: {
+    title: 'app.header.title',
+    themeSwitcher: 'app.header.themeSwitcher',
+    changeTheme: 'app.header.changeTheme',
+    userFilter: {
+      label: 'app.header.userFilter.label',
+      all: 'app.header.userFilter.all',
+      paid: 'app.header.userFilter.paid',
+      pending: 'app.header.userFilter.pending',
+    },
+  },
+  snackBar: {
+    close: 'app.snackBar.close',
+  },
+};
