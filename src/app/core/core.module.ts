6d899822fcbdb0a3967b84b1e99a3733d5f73df0
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-15 15:38:18 -0300

    chore(core-module): add translate module config

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,16 +1,43 @@
-import { FormControlValidatorService } from '@core/services/form-control-validator/form-control-validator.service';
-import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { LANGUAGES, ptLang } from '@core/languages/languages.lang';
 import { ApiService } from '@core/services/api/api.service';
-import { ThemeService } from './services/theme/theme.service';
+import { FormControlValidatorService } from '@core/services/form-control-validator/form-control-validator.service';
+import { ThemeService } from '@core/services/theme/theme.service';
+import { TranslateSwicherService } from '@core/services/translate-switcher/translate-switcher.service';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function httpTranslateLoader(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
-  imports: [HttpClientModule],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpTranslateLoader,
+        deps: [HttpClient],
+      },
+    }),
+  ],
+  exports: [CommonModule, TranslateModule],
   providers: [
     HttpClientModule,
     ApiService,
     ThemeService,
+    TranslateSwicherService,
     FormControlValidatorService,
   ],
 })
-export class CoreModule {}
+export class CoreModule {
+  constructor(translateService: TranslateService) {
+    translateService.addLangs(LANGUAGES);
+    translateService.setDefaultLang(ptLang);
+    translateService.use(ptLang);
+  }
+}
