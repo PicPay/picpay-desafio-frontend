a007256c8bfa3c42df13012986f5e79c8a1c3be2
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-15 16:24:42 -0300

    feat(transaction): add TRANSACTION_SERVICE_VOCABULARY file

---
+++ src/app/shared/services/transaction/transaction.service.vocabulary.ts
@@ -0,0 +1,5 @@
+export const TRANSACTION_SERVICE_VOCABULARY = {
+  success: 'shared.transaction.success',
+  invalidCard: 'shared.transaction.invalidCard',
+  errors: 'shared.transaction.errors',
+};
