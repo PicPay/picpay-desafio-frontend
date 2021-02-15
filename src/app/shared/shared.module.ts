5b88de9982eacf24803b7011f7667609b5b13ed2
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-15 16:25:10 -0300

    chore(shared): remove unused module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,9 @@
-import { CoreModule } from '@core/core.module';
 import { NgModule } from '@angular/core';
 import { ComponentsModule } from '@shared/components/components.module';
 import { TransactionService } from '@shared/services/transaction/transaction.service';
 import { UserService } from '@shared/services/user/user.service';
 
 @NgModule({
-  imports: [CoreModule],
   exports: [ComponentsModule],
   providers: [TransactionService, UserService],
 })
