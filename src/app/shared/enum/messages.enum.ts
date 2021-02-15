9519b972f20dbcf1998e987504430decf5f1369b
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-14 21:30:48 -0300

    refactor(shared): add messages enum

---
+++ src/app/shared/enum/messages.enum.ts
@@ -0,0 +1,8 @@
+export const enum ErrorMessage {
+  INVALID_CARD = 'Não foi possível fazer a transação, número de cartão inválido',
+  SERVER_ERROR = 'Ocorreu um erro no servidor',
+}
+
+export const enum SuccessMessage {
+  TRANSACTION_SUCCESS = 'foi pago com sucesso',
+}
