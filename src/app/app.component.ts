452a0ec07e7b7b37558b9c7ef69511e72acc3078
Bruno Quadrotti de Freitas <bruno.freitas@fluig.com>
2021-02-16 11:07:29 -0300

    refactor: refactoring to use card user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { FormPaymentComponent } from './components/form-payment/form-payment.component';
-import { TransactionResponse } from './models/transaction-response.interface';
 import { User } from './models/user.interface';
-import { ModalService } from './services/modal.service';
 import { UserService } from './services/user.service';
 
 @Component({
@@ -14,41 +11,13 @@ import { UserService } from './services/user.service';
 })
 export class AppComponent implements OnInit {
 
-  @ViewChild('paymentMessageResponse', { static: true })
-  private paymentMessageResponseTpl: TemplateRef<any>;
-
-  title = 'Desafio Picpay Front-end';
   users: Observable<User[]>;
-  showModalPayment = false;
-  showModalPaymentMessage = false;
-  paymentStatus: 'Aprovada' | 'Reprovada';
 
   constructor(
     private userService: UserService,
-    private modalService: ModalService,
   ) { }
 
   ngOnInit(): void {
     this.users = this.userService.getUsers();
-  }
-
-  public payUserModal(user: User): void {
-    const modalTitle = `Pagamento para ${user.name}`
-    const modalRef = this.modalService.open(FormPaymentComponent, modalTitle, user);
-
-    modalRef.afterClosed$.subscribe((res) => {
-      if(res && res.data) {
-        this.showModalPaymentMessageResponse(res.data);
-      }
-    });
-  }
-
-  public showModalPaymentMessageResponse(transaction: TransactionResponse): void {
-    this.paymentStatus = transaction.status;
-    this.modalService.open(
-      this.paymentMessageResponseTpl,
-      'Recibo de pagamento',
-      null
-    );
   }
 }
