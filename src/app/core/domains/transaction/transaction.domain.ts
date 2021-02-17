13f0bd4f71b510ec9727b15f88a53cd65dacc891
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-17 12:40:44 -0300

    fix(domain): edit trasaction domain

--- src/app/core/domains/transaction/transaction.domain.ts
+++ src/app/core/domains/transaction/transaction.domain.ts
@@ -1,10 +1,4 @@
-import { User } from '@core/domains/user/user.domain';
-
 export interface Transaction {
-  id: number;
-  timestamp: number;
-  value: number;
-  destination_user: User;
   success: boolean;
   status: string;
 }
