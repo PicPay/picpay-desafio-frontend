ea6e5d1f081bf23e311616978a5ebb48b430dd5e
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-17 12:38:57 -0300

    test(transaction): test post transaction

--- src/app/shared/services/transaction/transaction.service.spec.ts
+++ src/app/shared/services/transaction/transaction.service.spec.ts
@@ -1,12 +1,12 @@
-import { MOCK_INVALID_CARD } from './../../mocks/card/card.mock';
 import { TestBed } from '@angular/core/testing';
 import { ApiService } from '@core/services/api/api.service';
 import { TranslateService } from '@ngx-translate/core';
 import {
-  MOCK_TRANSACTION_API_RESULT,
-  MOCK_TRANSACTION_PAYLOAD,
+  MOCK_TRANSACTION,
+  MOCK_TRANSACTION_PAYLOAD
 } from '@shared/mocks/transaction/transaction.mock';
 import { of } from 'rxjs';
+import { MOCK_INVALID_CARD } from './../../mocks/card/card.mock';
 import { TransactionService } from './transaction.service';
 import { TRANSACTION_SERVICE_VOCABULARY } from './transaction.service.vocabulary';
 
@@ -30,15 +30,11 @@ describe('TransactionService', () => {
   });
 
   it('should send post transaction', () => {
-    apiServiceSpy.post.and.returnValue(of(MOCK_TRANSACTION_API_RESULT));
+    apiServiceSpy.post.and.returnValue(of(MOCK_TRANSACTION));
     transactionService
       .postTransaction(MOCK_TRANSACTION_PAYLOAD)
       .subscribe((transaction) => {
-        expect(transaction).toBeTruthy();
-        expect(typeof transaction).toBe('object');
-        expect(transaction.destination_user.id).toEqual(
-          MOCK_TRANSACTION_PAYLOAD.destination_user_id
-        );
+        expect(transaction.success).toBeTruthy();
       });
   });
 
