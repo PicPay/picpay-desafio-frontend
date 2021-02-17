14139b39b0f97857fa92644ff939af3ab8a20083
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-17 12:39:22 -0300

    chore(mocks): remove transaction api result mock

--- src/app/shared/mocks/transaction/transaction.mock.ts
+++ src/app/shared/mocks/transaction/transaction.mock.ts
@@ -1,6 +1,5 @@
 import { Transaction } from '@core/domains/transaction/transaction.domain';
 import { TransactionPayload } from '@core/domains/transaction/transaction-payload.domain';
-import { TransactionAPIResult } from '@shared/services/transaction/transaction.service';
 
 export const MOCK_TRANSACTION_PAYLOAD: TransactionPayload = {
   card_number: '1111111111111111',
@@ -10,32 +9,7 @@ export const MOCK_TRANSACTION_PAYLOAD: TransactionPayload = {
   value: 11111,
 };
 
-export const MOCK_TRANSACTION_API_RESULT: TransactionAPIResult = {
-  transaction: {
-    id: 1,
-    timestamp: 1111111,
-    value: 11111,
-    destination_user: {
-      id: 1,
-      img: 'https://fakeimg.pl/300/',
-      name: 'mock',
-      username: 'mock@mock',
-    },
-    success: true,
-    status: '200',
-  },
-};
-
 export const MOCK_TRANSACTION: Transaction = {
-  id: 1,
-  timestamp: 1111111,
-  value: 11111,
-  destination_user: {
-    id: 1,
-    img: 'https://fakeimg.pl/300/',
-    name: 'mock',
-    username: 'mock@mock',
-  },
   success: true,
   status: '200',
 };
