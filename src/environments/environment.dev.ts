24c34b6d45bff40a6d755f73189ca70e98c6ead5
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2021-02-17 12:41:18 -0300

    fix(env): remove comments on env file

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,10 +1,5 @@
 export const environment = {
   production: false,
   baseUsersApiUrl: 'https://www.mocky.io/v2/',
-
-  /* ESTÁ RETORNANDO CORS ERROR */
-  // baseTransactionApiUrl: '/api/'
-
-  /* RETORNA SOMENTE O USER { id: 1002  name: "Marina Coelho" } independente do ID passado*/
-  baseTransactionApiUrl: 'https://www.mocky.io/v2/',
+  baseTransactionApiUrl: 'https://run.mocky.io/v3/'
 };
