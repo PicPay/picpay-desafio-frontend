8a1ab5085a9ccbc77a0f1cbd086a16a8cacace9f
Carolynne Padilha da Silva <carodilha@gmail.com>
2021-02-23 21:57:48 -0300

    Cleaned main component and added new modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UserCardComponent } from './components/users/user-card/user-card.component';
 import { UsersListComponent } from './components/users/users-list/users-list.component';
+import { TransactionCardComponent } from './components/transaction/transaction-card/transaction-card.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserCardComponent,
-    UsersListComponent
+    UsersListComponent,
+    TransactionCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
