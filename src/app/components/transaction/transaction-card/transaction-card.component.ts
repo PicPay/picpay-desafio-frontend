fe2c508b427a553a6635c66eee5faebdcd7112ef
Carolynne Padilha da Silva <carodilha@gmail.com>
2021-02-24 00:31:55 -0300

    Added payment validation.

--- src/app/components/transaction/transaction-card/transaction-card.component.ts
+++ src/app/components/transaction/transaction-card/transaction-card.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { CARDS } from 'src/app/shared/mock/card.mock';
 import { User } from 'src/app/shared/models/user.model';
+import { TransactionService } from 'src/app/shared/services/transaction.service';
 
 @Component({
   selector: 'app-transaction-card',
@@ -15,13 +17,22 @@ export class TransactionCardComponent implements OnInit {
 
   public availableCards: any[] = CARDS;
 
+  public success: boolean = false;
+
+  public validCardRegex: RegExp = /^[1]{16}$/;
+
   public formGroup = new FormGroup({
     value: new FormControl(0, [
+      Validators.required
+    ]),
+    cardNumber: new FormControl('Selecione', [
       Validators.required
     ])
   });
 
-  constructor() { }
+  constructor(
+    private toastr: ToastrService,
+    private transactionService: TransactionService) { }
 
   ngOnInit() {
   }
@@ -30,4 +41,42 @@ export class TransactionCardComponent implements OnInit {
     return cardNumber.trim().substr(cardNumber.length - 4);
   }
 
+  pay(recipientUserId: number) {
+    const value = this.formGroup.controls['value'].value;
+
+    const cardNumber = this.formGroup.controls['cardNumber'].value;
+
+    if (!this.isPaymentValid(value, cardNumber)) {
+      this.success = false;
+    }
+
+    this.transactionService.payUser(recipientUserId, value).subscribe((result) => {
+      if (result['success'])
+        this.success = true;
+    });
+  }
+
+  private isPaymentValid(value: number, cardNumber: string) {
+    const title = "Houston, we have a problem.";
+
+    if (!value) {
+      this.toastr.error("Ei! Você não pode pagar \"nada\" para uma pessoa.", title);
+
+      return false;
+    }    
+
+    if (!cardNumber || cardNumber.length !== 16) {
+      this.toastr.error("Por favor, verifique o número disc... O cartão informado e tente novamente.", title);
+
+      return false;
+    }
+
+    if (!this.validCardRegex.test(cardNumber)) {
+      this.toastr.error("O cartão informado é inválido! :-(", title);
+
+      return false;
+    }
+
+    return true;
+  }
 }
