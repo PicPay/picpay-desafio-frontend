64f50e013a7d7646d0edaaa2684b9f92c865dc11
Carolynne Padilha da Silva <carodilha@gmail.com>
2021-02-24 22:25:39 -0300

    Mock.

---
+++ src/app/shared/mock/bank-statement.mock.ts
@@ -0,0 +1,7 @@
+export const BANK_STATEMENTS: TransactionPayload[] = [
+    { expiry_date: '', cvv: 0, card_number: '1234123412341234', destination_user_id: 0, value: 20 },
+    { expiry_date: '', cvv: 0, card_number: '1234123412341111', destination_user_id: 0, value: 19.89 },
+    { expiry_date: '', cvv: 0, card_number: '1234123412349999', destination_user_id: 0, value: 5.99 },
+    { expiry_date: '', cvv: 0, card_number: '1234123412341234', destination_user_id: 0, value: 0.5 },
+    { expiry_date: '', cvv: 0, card_number: '1234123412341234', destination_user_id: 0, value: 15 },
+]
