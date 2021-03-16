da6ee5e6d7c389f5026fa330882dd5942625d6ab
Cesar Augusto <gutoamorim@outlook.com>
2021-03-16 19:21:04 -0300

    update mask

--- src/app/views/user-payment/user-payment.component.ts
+++ src/app/views/user-payment/user-payment.component.ts
@@ -1,7 +1,7 @@
 import { User } from './../../components/user/user.model';
 import { HttpClient } from '@angular/common/http';
 import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-payment',
@@ -29,8 +29,7 @@ export class UserPaymentComponent implements OnInit {
 
   constructor(
     private hhtp: HttpClient,
-    public dialogRef: MatDialogRef<UserPaymentComponent>, 
-  @Inject(MAT_DIALOG_DATA) public data:  User  ) {}
+    public dialogRef: MatDialogRef<UserPaymentComponent>, @Inject(MAT_DIALOG_DATA) public data:  User  ) {}
 
   ngOnInit() {
   }
