a1f5d5b59c2c6ca1d611ff719914a58a64941c5a
lucasbarrosc <costa.lucasbarros@gmail.com>
2021-03-19 18:25:18 -0300

    Created User List Item Component

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { UserListItemComponent } from "./user-list-item/user-list-item.component";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [UserListItemComponent],
+  imports: [CommonModule, SharedModule],
 })
 export class UserModule {}
