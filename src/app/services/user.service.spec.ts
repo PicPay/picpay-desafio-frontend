5595de5dcb594785224b410b6bb099cc6ae48c13
Flavio Duarte <flduarte13@gmail.com>
2021-03-20 21:49:30 -0300

    test: ensure UserService.getUser$ calls HttpClient with the correct url

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,12 +1,36 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 
 import { UserService } from './user.service';
 
 describe('UserService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let httpClient: HttpClient;
+  let service: UserService;
+  
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [
+        { provide: 'USERS_URL', useValue: 'www.fake.com'}
+      ]
+    });
+    service = TestBed.get(UserService);
+    httpClient = TestBed.get(HttpClient);
+  });
 
   it('should be created', () => {
-    const service: UserService = TestBed.get(UserService);
     expect(service).toBeTruthy();
+  });
+
+  describe('getUsers$', () => {
+    it('should call HttpClient with the correct url', () => {
+      const fakeUrl = TestBed.get('USERS_URL')
+      spyOn(httpClient, 'get')
+
+      service.getUsers$()
+
+      expect(httpClient.get).toHaveBeenCalledWith(fakeUrl)
+    });
   });
 });
