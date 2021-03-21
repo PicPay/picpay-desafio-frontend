419d69ddfddca7a935fe41904eeab68f46a01095
lucasbarrosc <costa.lucasbarros@gmail.com>
2021-03-21 15:27:55 -0300

    Added Tests for the User List Component

--- src/app/user/user-list/user-list.component.spec.ts
+++ src/app/user/user-list/user-list.component.spec.ts
@@ -1,16 +1,46 @@
+import { Component, Input } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDividerModule, MatProgressSpinnerModule } from '@angular/material';
+import { of } from 'rxjs';
+import { User } from 'src/app/shared/models/user.model';
+import { UserService } from 'src/app/shared/services/user/user.service';
 
 import { UserListComponent } from './user-list.component';
+
+@Component({ selector: 'app-user-list-item', template: '' })
+class UserListItemStubComponent {
+  @Input() user: User;
+}
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;
+  let userServiceSpy: jasmine.SpyObj<UserService>;
+  let testUsers = [
+    {
+      id: 0,
+      name: 'Test User 1',
+      username: '@test-user-1',
+      img: 'test-user-1-image-path',
+    },
+    {
+      id: 1,
+      name: 'Test User 2',
+      username: '@test-user-2',
+      img: 'test-user-2-image-path',
+    },
+  ];
 
   beforeEach(async(() => {
+    userServiceSpy = jasmine.createSpyObj('UserService', ['getUsers']);
+
+    userServiceSpy.getUsers.and.returnValue(of(testUsers));
+
     TestBed.configureTestingModule({
-      declarations: [ UserListComponent ]
-    })
-    .compileComponents();
+      imports: [MatProgressSpinnerModule, MatDividerModule],
+      declarations: [UserListComponent, UserListItemStubComponent],
+      providers: [{ provide: UserService, useValue: userServiceSpy }],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +51,13 @@ describe('UserListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the correct ammount of User List Item Components', () => {
+    let listItems = fixture.nativeElement.querySelectorAll(
+      'app-user-list-item'
+    );
+
+    expect(listItems.length).toBe(testUsers.length, 'same length');
   });
 });
