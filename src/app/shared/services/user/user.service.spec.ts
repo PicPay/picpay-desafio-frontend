9d38f767875289df8b5f897ce44682d23f8bb2e9
lucasbarrosc <costa.lucasbarros@gmail.com>
2021-03-21 15:08:08 -0300

    Updated User Service and It's Tests

--- src/app/shared/services/user/user.service.spec.ts
+++ src/app/shared/services/user/user.service.spec.ts
@@ -1,27 +1,31 @@
-import { HttpClient } from "@angular/common/http";
-import { TestBed } from "@angular/core/testing";
-import { of } from "rxjs";
-import { User } from "../../models/user.model";
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { async, TestBed } from '@angular/core/testing';
+import { of, throwError } from 'rxjs';
+import { User } from '../../models/user.model';
 
-import { UserService } from "./user.service";
+import { UserService } from './user.service';
 
-describe("UserService", () => {
+describe('UserService', () => {
   let httpClientSpy: jasmine.SpyObj<HttpClient>;
   let userService: UserService;
 
   beforeEach(() => {
-    httpClientSpy = jasmine.createSpyObj("HttpClient", ["get"]);
-    userService = new UserService(httpClientSpy);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+
+    TestBed.configureTestingModule({
+      providers: [{ provide: HttpClient, useValue: httpClientSpy }],
+    });
+    userService = TestBed.get(UserService);
   });
 
-  it("should be created", () => {
+  it('should be created', () => {
     expect(userService).toBeTruthy();
   });
 
-  it("#getUsers should return an Observable of the Users from the response", () => {
+  it('#getUsers should return an Observable of the Users from the response', () => {
     const users: User[] = [
-      { id: 1, name: "User1", img: "user1.png", username: "user1" },
-      { id: 2, name: "User2", img: "user2.png", username: "user2" },
+      { id: 1, name: 'User1', img: 'user1.png', username: 'user1' },
+      { id: 2, name: 'User2', img: 'user2.png', username: 'user2' },
     ];
 
     httpClientSpy.get.and.returnValue(of(users));
@@ -29,10 +33,23 @@ describe("UserService", () => {
     userService
       .getUsers()
       .subscribe(
-        (heroes) => expect(heroes).toEqual(users, "expected users"),
+        (heroes) => expect(heroes).toEqual(users, 'expected users'),
         fail
       );
 
-    expect(httpClientSpy.get.calls.count()).toBe(1, "one call");
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
   });
+
+  it('#getUsers should return an Observable with an empty array when the server returns an error', async(() => {
+    const errorResponse = new HttpErrorResponse({});
+
+    httpClientSpy.get.and.returnValue(throwError(errorResponse));
+
+    userService.getUsers().subscribe((users) => {
+      console.log('in test', users);
+      expect(users).toEqual([], 'empty');
+    });
+
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+  }));
 });
--- src/app/shared/services/user/user.service.ts
+++ src/app/shared/services/user/user.service.ts
@@ -1,21 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { User } from '../../models/user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-  private static URL = "https://www.mocky.io/v2/5d531c4f2e0000620081ddce";
+  private static URL = 'https://www.mocky.io/v2/5d531c4f2e0000620081ddce';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   /**
-   * Get users from the API
-   * @returns {User[]}
+   * Get users from the API.
+   * @returns {Observable<User[]>} An observable that resolves to an array of users.
+   * The observable will resolve to an empty array if an error occurred.
    */
   public getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(UserService.URL);
+    return this.http.get<User[]>(UserService.URL).pipe(catchError(() => []));
   }
 }
