0977bfd33c5e3548d311e743e611e995fdad323f
Flavio Duarte <flduarte13@gmail.com>
2021-03-21 19:16:49 -0300

    test: ensure UserListComponent calls UserService.getUsers$ when component starts

--- src/app/components/user-list/user-list.component.spec.ts
+++ src/app/components/user-list/user-list.component.spec.ts
@@ -1,25 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { spyOnClass } from 'jasmine-es6-spies';
+import { UserService } from 'src/app/services/user.service';
 
 import { UserListComponent } from './user-list.component';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;
+  let userService: jasmine.SpyObj<UserService>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserListComponent ]
+      declarations: [ UserListComponent ],
+      providers: [
+        { provide: UserService, useFactory: () => spyOnClass(UserService) }
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UserListComponent);
+    userService = TestBed.get(UserService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call UserService.getUsers$ when component starts', () => {
+    expect(userService.getUsers$).toHaveBeenCalled();
   });
 });
