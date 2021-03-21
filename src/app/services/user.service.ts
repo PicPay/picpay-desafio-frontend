8f63790cf130fbd0c4f9c970ab8557c2b81359c6
Flavio Duarte <flduarte13@gmail.com>
2021-03-20 21:49:12 -0300

    feat: ensure UserService.getUser$ calls HttpClient with the correct url

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  constructor(
+    @Inject('USERS_URL') private _url: string,
+    private _httpClient: HttpClient
+  ) {}
 
-  constructor() { }
+  getUsers$() {
+    return this._httpClient.get(this._url)
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, StaticProvider } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -8,5 +8,9 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+const providers: StaticProvider[] = [
+  { provide: 'USERS_URL', useValue: 'https://www.mocky.io/v2/5d531c4f2e0000620081ddce' }
+]
+
+platformBrowserDynamic(providers).bootstrapModule(AppModule)
   .catch(err => console.error(err));
