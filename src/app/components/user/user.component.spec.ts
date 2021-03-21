0d0e600408906c08ce9d5d0dbed8eda44bae08c4
Flavio Duarte <flduarte13@gmail.com>
2021-03-21 18:15:10 -0300

    test: ensure UserComponent calls ModalService.open when "Pagar" button is clicked

--- src/app/components/user/user.component.spec.ts
+++ src/app/components/user/user.component.spec.ts
@@ -1,25 +1,48 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ModalService } from 'src/app/services/modal.service';
+import { spyOnClass } from 'jasmine-es6-spies';
 
 import { UserComponent } from './user.component';
 
 describe('UserComponent', () => {
   let component: UserComponent;
   let fixture: ComponentFixture<UserComponent>;
+  let modalService: jasmine.SpyObj<ModalService>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserComponent ]
+      declarations: [ UserComponent ],
+      providers: [
+        { provide: ModalService, useFactory: () => spyOnClass(ModalService) }
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UserComponent);
+    modalService = TestBed.get(ModalService)
     component = fixture.componentInstance;
+
+    component.user = {
+      id: 1,
+      name: 'Flavio Lopes Duarte',
+      username: 'duarte.flavio',
+      img: 'https://randomuser.me/api/portraits/men/9.jpg'
+    }
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call ModalService.open when "Pagar" button is clicked', () => {
+    const button = fixture.nativeElement.querySelector('.pay-button');
+
+    button.click();
+
+    expect(modalService.open).toHaveBeenCalled();
   });
 });
