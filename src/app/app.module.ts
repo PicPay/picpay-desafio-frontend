eebc12c9770fa8922c920c429bb75df1b870b687
nandabalassoni <mariafernandabalassoni@hotmail.com>
2021-03-22 16:33:25 -0300

    importação componente e rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PagamentoComponent } from './pagamento/pagamento.component';
+
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagamentoComponent
   ],
   imports: [
     BrowserModule
