e10a6b4fdb0d2f5d5b0d8ddde604f3e41da04dd5
nandabalassoni <mariafernandabalassoni@hotmail.com>
2021-03-22 18:48:00 -0300

    excluindo coisas do service que não vou utilizar

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -9,15 +8,7 @@ export class UserService {
 
   baseUrl = 'https://www.mocky.io/v2/5d531c4f2e0000620081ddce';
 
-  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
-
-  showMessage(msg: string): void {
-    this.snackBar.open(msg, 'x', {
-      duration: 3000,
-      horizontalPosition: 'right',
-      verticalPosition: 'top'
-    })
-  };
+  constructor(private http: HttpClient) { }
 
   listar() {
     return this.http.get<any[]>(`${this.baseUrl}`);
