aad859abdf02310f477df676a9f765546889abe9
Flavio Duarte <flduarte13@gmail.com>
2021-03-22 20:43:49 -0300

    test: ensure ModalContainerComponent calls ModalService.getActiveModal$ when component starts

--- src/app/components/modal-container/modal-container.component.spec.ts
+++ src/app/components/modal-container/modal-container.component.spec.ts
@@ -1,25 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { spyOnClass } from 'jasmine-es6-spies';
+import { ModalService } from 'src/app/services/modal.service';
 
 import { ModalContainerComponent } from './modal-container.component';
 
 describe('ModalContainerComponent', () => {
   let component: ModalContainerComponent;
   let fixture: ComponentFixture<ModalContainerComponent>;
+  let modalService: jasmine.SpyObj<ModalService>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ModalContainerComponent ]
+      declarations: [ ModalContainerComponent ],
+      providers: [
+        { provide: ModalService, useFactory: () => spyOnClass(ModalService) }
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ModalContainerComponent);
+    modalService = TestBed.get(ModalService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call ModalService.getActiveModal$ when component starts', () => {
+    expect(modalService.getActiveModal$).toHaveBeenCalled();
   });
 });
