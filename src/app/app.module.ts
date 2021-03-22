4f137df8ab7dd151ca6978712d6e2753994cc21a
nandabalassoni <mariafernandabalassoni@hotmail.com>
2021-03-22 17:43:35 -0300

    fazendo a importacao do componente no modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { PagamentoComponent } from './pagamento/pagamento.component';
 import { AppRoutingModule } from './app-routing.module';
 import { UsersComponent } from './users/users.component';
+import { ConfirmacaoComponent } from './confirmacao/confirmacao.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PagamentoComponent,
-    UsersComponent
+    UsersComponent,
+    ConfirmacaoComponent
   ],
   imports: [
     BrowserModule,
