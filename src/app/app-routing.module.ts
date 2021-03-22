e378846e7c0d30a603fb9320c55c8c1a512a9a7a
nandabalassoni <mariafernandabalassoni@hotmail.com>
2021-03-22 17:54:33 -0300

    adicionando a rota para tela de confirmação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
+import { ConfirmacaoComponent } from './confirmacao/confirmacao.component';
 import { PagamentoComponent } from './pagamento/pagamento.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 const routes: Routes = [
-  { path: 'pagamentos', component: PagamentoComponent },];
+  { path: 'pagamentos', component: PagamentoComponent },
+  { path: 'confirmacao', component: ConfirmacaoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)], /* O método é chamado forRoot()porque você configura o roteador no nível raiz do aplicativo. O forRoot()método fornece os provedores de serviço e as diretivas necessárias para o roteamento e executa a navegação inicial com base na URL do navegador atual.*/
