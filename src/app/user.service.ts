3225bc052ea486c7b1501422ec988f506e2c6929
nandabalassoni <mariafernandabalassoni@hotmail.com>
2021-03-22 18:34:39 -0300

    adicionando serviços de usuario

---
+++ src/app/user.service.ts
@@ -0,0 +1,23 @@
+import { User } from './users/users.model';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  baseUrl = 'https://www.mocky.io/v2/5d531c4f2e0000620081ddce';
+
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
+
+  showMessage(msg: string): void {
+    this.snackBar.open(msg, 'x', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top'
+    })
+  }
+}
