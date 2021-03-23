1d6dbef3e05963deb9417435f47b224b9a66a136
Rayssa Araújo <rayssa.marujo@gmail.com>
2021-03-23 20:50:20 -0300

    feat: Criando modal

---
+++ src/app/public/modal/modal.component.ts
@@ -0,0 +1,54 @@
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnDestroy,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
+
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+
+export class ModalComponent implements OnInit, OnDestroy {
+ 
+  @Input() id: string;
+  private element: any;
+
+  constructor(private modalService: ModalService, private el: ElementRef) {
+    this.element = el.nativeElement;
+  }
+
+  ngOnInit(): void {
+    document.body.appendChild(this.element);
+
+    this.element.addEventListener('click', (el) => {
+      if (el.target.className === 'app-modal') {
+        this.close();
+      }
+    });
+
+    this.modalService.add(this);
+  }
+
+  ngOnDestroy(): void {
+    this.modalService.remove(this.id);
+    this.element.remove();
+  }
+
+  open(): void {
+    this.element.style.display = 'block';
+    document.body.classList.add('modal__open');
+  }
+
+  close(): void {
+    this.element.style.display = 'none';
+    document.body.classList.remove('modal__open');
+  }
+}
