2e7a4061b30e711e4cc8245da64b1c9ed368fe10
Flavio Duarte <flduarte13@gmail.com>
2021-03-23 12:47:39 -0300

    feat: add PaymentModalComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,22 @@ import { UserComponent } from './components/user/user.component';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ModalContainerComponent } from './components/modal-container/modal-container.component';
+import { PaymentModalComponent } from './components/payment-modal/payment-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     UserListComponent,
-    ModalContainerComponent
+    ModalContainerComponent,
+    PaymentModalComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule
   ],
   providers: [],
+  entryComponents: [ PaymentModalComponent ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
