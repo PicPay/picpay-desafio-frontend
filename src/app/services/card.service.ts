f81a6445940f1c28972a14e7b77ce5fee124c7a8
Flavio Duarte <flduarte13@gmail.com>
2021-03-24 12:42:05 -0300

    feat: ensure CardService returns a list of cards

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -7,10 +7,22 @@ import { CardModel } from '../models/card-model';
   providedIn: 'root'
 })
 export class CardService {
+  cards: CardModel[] = [
+    {
+      cardNumber: '1111111111111111',
+      cvv: 789,
+      expiryDate: '01/18'
+    },
+    {
+      cardNumber: '4111111111111234',
+      cvv: 123,
+      expiryDate: '01/20'
+    }
+  ];
 
   constructor() { }
 
   getCards$(): Observable<CardModel[]> {
-    return of(null);
+    return of(this.cards);
   }
 }
