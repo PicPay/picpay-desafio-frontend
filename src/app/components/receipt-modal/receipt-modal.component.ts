187ff3831f162210ebdc95c0a6827037bdf1fc98
Flavio Duarte <flduarte13@gmail.com>
2021-03-24 19:18:14 -0300

    feat: ensure ReceiptModalComponent renders a fail message when fails

--- src/app/components/receipt-modal/receipt-modal.component.ts
+++ src/app/components/receipt-modal/receipt-modal.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-receipt-modal',
   templateUrl: './receipt-modal.component.html',
   styleUrls: ['./receipt-modal.component.scss']
 })
-export class ReceiptModalComponent implements OnInit {
+export class ReceiptModalComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(@Inject('MODAL_DATA') public success: any) { }
 
 }
