0d0fa743691d16cd49c6eee0bdb59a17401a07db
Flavio Duarte <flduarte13@gmail.com>
2021-03-24 12:42:14 -0300

    test: ensure CardService returns a list of cards

--- src/app/services/card.service.spec.ts
+++ src/app/services/card.service.spec.ts
@@ -1,12 +1,25 @@
 import { TestBed } from '@angular/core/testing';
+import { CardModel } from '../models/card-model';
 
 import { CardService } from './card.service';
 
 describe('CardService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: CardService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({})
+    service = TestBed.get(CardService);
+  });
 
   it('should be created', () => {
-    const service: CardService = TestBed.get(CardService);
     expect(service).toBeTruthy();
+  });
+
+  it('should return a list of cards', () => {
+    const spy = jasmine.createSpy('spy');
+
+    service.getCards$().subscribe(spy);
+    
+    expect(spy).toHaveBeenCalledWith(service.cards);
   });
 });
