c8b04762c0886ca4e66a2ae4270a897fddd8f5b3
Daniel <ubuntu@DESKTOP-9314L0B.localdomain>
2021-03-26 10:23:44 -0300

    refactor: organizes the structure of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { UserContainerComponent } from './users/user-container/user-container.component';
+import { UserDialogComponent } from './users/user-dialog/user-dialog.component';
+import { PaymentModalComponent } from './users/user-dialog/payment-modal/payment-modal.component';
+import { MessageModalComponent } from './users/user-dialog/message-modal/message-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserContainerComponent,
+    UserDialogComponent,
+    PaymentModalComponent,
+    MessageModalComponent,
   ],
   imports: [
     BrowserModule
