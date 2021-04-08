aed843530d6c0317d51242c63fec78b2b507b059
alessandra.nsantos <alessandra.nsantos@b2wdigital.com>
2021-04-07 21:34:52 -0300

    payment: initial structure 🔧

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ModalComponent } from './utils/modal/modal.component';
+import { UsersComponent } from './payment/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
