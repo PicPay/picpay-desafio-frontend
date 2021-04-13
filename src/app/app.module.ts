14a04c306c6b8b8624c5af94f48e1c741bcb0fb0
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-04-13 19:11:56 -0300

    feat: add user card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from  '@angular/material/button';
+
 import { FooterComponent } from './components/template/footer/footer.component';
 import { MainComponent } from './components/template/main/main.component';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainComponent
+    MainComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
