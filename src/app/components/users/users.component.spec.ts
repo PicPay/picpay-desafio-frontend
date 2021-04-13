dcb891510a28790adc9f2001561eaffa8f1a3f0c
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-04-13 19:42:00 -0300

    test: update Users Component unit test

--- src/app/components/users/users.component.spec.ts
+++ src/app/components/users/users.component.spec.ts
@@ -2,6 +2,9 @@ import { MatCardModule } from '@angular/material/card';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UsersComponent } from './users.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { UsersService } from 'src/app/services/users.service';
+import { of } from 'rxjs';
 
 describe('UsersComponent', () => {
   let component: UsersComponent;
@@ -12,12 +15,32 @@ describe('UsersComponent', () => {
       declarations: [ UsersComponent ],
       imports: [
         MatCardModule,
-      ]
+        HttpClientTestingModule
+      ],
+      providers: [UsersService]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    const userService = TestBed.get(UsersService);
+
+    spyOn(userService, "getUsers").and.returnValue(
+      of([
+        {
+          "id": 1001,
+          "name": "Eduardo Santos",
+          "img": "https://randomuser.me/api/portraits/men/9.jpg",
+          "username": "@eduardo.santos"
+        },
+        {
+          "id": 1002,
+          "name": "Marina Coelho",
+          "img": "https://randomuser.me/api/portraits/women/37.jpg",
+          "username": "@marina.coelho"
+        }
+      ])
+    );
     fixture = TestBed.createComponent(UsersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
