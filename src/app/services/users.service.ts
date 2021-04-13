b64d94f6b19e1bfc60c70ba5b49e26121926a3ab
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-04-13 19:26:48 -0300

    feat: add users service

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../shared/models/user.model';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
   
+  getUsers(): Observable<User[]> {
+    const url = `${environment.baseUrl}/v2/5d531c4f2e0000620081ddce`;
+    return this.http.get<User[]>(url);
+  }
 }
