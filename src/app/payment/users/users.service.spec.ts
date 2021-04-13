992d280caa9b1c7332726d06dbeda4cc951ab4db
alessandra.nsantos <alessandra.nsantos@b2wdigital.com>
2021-04-12 21:58:05 -0300

    payment: testing service user

--- src/app/payment/users/users.service.spec.ts
+++ src/app/payment/users/users.service.spec.ts
@@ -1,12 +1,25 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let usersService = UsersService;
+
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientModule, HttpModule],
+    providers: [UsersService]
+  }));
 
   it('should be created', () => {
     const service: UsersService = TestBed.get(UsersService);
     expect(service).toBeTruthy();
   });
+
+  it('should be listed users', inject([UsersService], async (usersService) => {
+    const result = await usersService.getUsers()
+    expect(result).not.toBeNull()
+  })
+  )
 });
