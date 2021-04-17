2aa12041aa7b62ca9588064add78c3b0ea4ea012
Luana Mozer <luanamozzer1996@gmail.com>
2021-04-17 10:04:36 -0300

    listando os usuarios com metodo GET

--- src/app/list-users/list-users.component.ts
+++ src/app/list-users/list-users.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ListUsersService } from './list-users.service';
+import { Users } from './users';
+
 
 @Component({
   selector: 'app-list-users',
@@ -6,10 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-users.component.scss']
 })
 export class ListUsersComponent implements OnInit {
+  users: Users[] ;
+  users$: Observable<Users[]>;
 
-  constructor() { }
+  constructor(private service: ListUsersService) { }
 
   ngOnInit() {
+    this.users$ = this.service.lista();
+  }
+  openModal() {
+    alert('');
   }
 
 }
