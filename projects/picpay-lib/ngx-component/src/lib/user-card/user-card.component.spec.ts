8afd12cbdabe5bd1cf9c6e770a524ab4b16ece1a
Richard Lindner <richard.lindner@fluig.com>
2021-04-18 11:58:14 -0300

    test(user-card): adiciona o teste do emmiter ao componente

--- projects/picpay-lib/ngx-component/src/lib/user-card/user-card.component.spec.ts
+++ projects/picpay-lib/ngx-component/src/lib/user-card/user-card.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { User, userOneMock } from '@picpay-lib/ngx-domain';
 
 import { UserCardComponent } from './user-card.component';
 
 describe('UserCardComponent', () => {
   let component: UserCardComponent;
   let fixture: ComponentFixture<UserCardComponent>;
+  const oneUser: User = new User(userOneMock);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,9 +18,16 @@ describe('UserCardComponent', () => {
     fixture = TestBed.createComponent(UserCardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    component.user = oneUser;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
+
+it('should emit the user then pay', () => {
+    const spyEmitter = spyOn(component.payToUser, 'emit');
+    component.pay();
+    expect(spyEmitter).toHaveBeenCalledWith(oneUser);
+  });
