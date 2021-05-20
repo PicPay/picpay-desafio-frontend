9b69c3dfacaa61e84f07b5eb27b444e200bc4df1
Talissa Andr <talissadeandrade@gmail.com>
2021-05-20 03:58:20 -0300

    (feature): makes possible to use modals in this application

---
+++ src/app/Modal/modal.module.ts
@@ -0,0 +1,12 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ModalComponent } from './modal.component';
+
+@NgModule({
+    imports: [CommonModule],
+    declarations: [ModalComponent],
+    exports: [ModalComponent],
+    schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+})
+export class ModalModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { UserItemComponent } from './UserItem/user-item.component';
+import { ModalModule } from './Modal/modal.module';
+import { PayFormComponent } from './PayForm/pay-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserItemComponent,
+    PayFormComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    ModalModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
 })
 export class AppModule { }
