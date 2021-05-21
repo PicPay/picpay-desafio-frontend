832a54609f6d85ed040ef812bc42adad06cfdd7c
Diego Carreira <diego.carreira@totvs.com.br>
2021-05-20 21:00:25 -0300

    Organizando estrutura base do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { UsersComponent } from './pages/users/users.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TransactionComponent } from './pages/dialogs/transaction/transaction.component';
+import { ConfirmComponent } from './pages/dialogs/confirm/confirm.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    TransactionComponent,
+    ConfirmComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
