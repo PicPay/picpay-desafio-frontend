7e6005c4ca9f695b4a2a03dd477807d33d2cfd7b
Allan <ariosmaia@yahoo.com.br>
2021-05-25 20:41:07 -0300

    Servico do modal

---
+++ src/app/@shared/components/modal/services/modal.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { ModalComponent } from './../modal.component';
+
+@Injectable()
+export class ModalService {
+	private modals: ModalComponent[] = []
+
+	add(modal: ModalComponent): void {
+		this.modals.push(modal);
+	}
+
+	remove(id: string): void {
+		this.modals = this.modals.filter(modal => modal.id !== id)
+	}
+
+	open(id: string): void {
+		this.openCloseModal(id, true);
+	} 
+
+	close(id: string): void {
+		this.openCloseModal(id, false);
+	} 
+
+	private openCloseModal(id: string,  state: boolean) {
+		console.log();
+		
+		const modal = this.modals.find(modal => modal.id === id);
+		if(!modal)  return;
+		modal.openModal(state);
+	}
+}
