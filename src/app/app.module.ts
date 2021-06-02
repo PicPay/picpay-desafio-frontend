f45fbfa39c29f43b1181752be9f2416ddd87ea7c
marcellevargas <marcelle.vargas96@gmail.com>
2021-06-02 13:24:17 -0300

    Migração do componente de user-list para a pasta de pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from "@angular/platform-browser";
 import { LOCALE_ID, NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
-import { UserListComponent } from "./user-list/user-list.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { PaymentModalComponent } from "./payment-modal/payment-modal.component";
@@ -20,6 +19,8 @@ import { HeaderComponent } from "./components/header/header.component";
 import { AvatarComponent } from "./components/avatar/avatar.component";
 import { PrimaryGreenButtonComponent } from "./components/primary-green-button/primary-green-button.component";
 import { HttpClientModule } from "@angular/common/http";
+import { UserListComponent } from "./pages/user-list/user-list.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 registerLocaleData(localePt, "pt");
 
@@ -40,7 +41,8 @@ registerLocaleData(localePt, "pt");
     MatIconModule,
     MatCardModule,
     MatButtonModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [
     { provide: LOCALE_ID, useValue: "pt" },
--- src/app/pages/user-list/user-list.component.ts
+++ src/app/pages/user-list/user-list.component.ts
@@ -1,24 +1,23 @@
-import { UsersService } from "./../services/users/users.service";
 import { Component, OnInit } from "@angular/core";
-import { User } from "../models/user";
+import { User } from "src/app/models/user";
+import { UsersService } from "src/app/services/users/users.service";
 
 @Component({
   selector: "app-user-list",
   templateUrl: "./user-list.component.html",
-  styleUrls: ["./user-list.component.scss"]
+  styleUrls: ["./user-list.component.scss"],
 })
+
 export class UserListComponent implements OnInit {
-  
   listUsers: User[];
   items;
-
 
   constructor(private usersService: UsersService) {
     this.usersService.requestUsers().subscribe((user) => {
       console.log(user);
       this.listUsers = user;
     });
-    this.items = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
+    this.items = Array.from({ length: 100000 }).map((_, i) => `Item #${i}`);
   }
 
   ngOnInit() {
