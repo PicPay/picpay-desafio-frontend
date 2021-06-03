c39d40921ea79a96fe8f38a4f2f71f8cecde04b5
marcellevargas <marcelle.vargas96@gmail.com>
2021-06-03 14:19:39 -0300

    Integração do componente de pagamento com o service de transação

--- src/app/pages/payment/payment.component.ts
+++ src/app/pages/payment/payment.component.ts
@@ -1,10 +1,10 @@
+import { TransactionService } from './../../services/transaction/transaction.service';
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { User } from "src/app/models/user";
 import { FormControl } from "@angular/forms";
 import { MatDialog } from "@angular/material/dialog";
 import { ListCardModalComponent } from "src/app/components/list-card-modal/list-card-modal.component";
-import { Card } from "src/app/models/card";
 
 @Component({
   selector: "app-payment",
@@ -16,7 +16,7 @@ export class PaymentComponent implements OnInit {
   user: User;
   valor = new FormControl("");
   card;
-  constructor(private route: ActivatedRoute, public dialog: MatDialog) {}
+  constructor(private route: ActivatedRoute, public dialog: MatDialog,private transaction:TransactionService) {}
 
   ngOnInit() {
     this.route.queryParamMap.subscribe((params) => {
@@ -29,7 +29,11 @@ export class PaymentComponent implements OnInit {
     let cardModal = this.dialog.open(ListCardModalComponent);
 
     cardModal.afterClosed().subscribe((card) => {
-      this.card = card.card_number;
+      this.card = card;
     });
+  }
+
+  sendPayment() {
+    this.transaction.formatData(this.user,this.valor,this.card)
   }
 }
---
+++ src/app/services/transaction/transaction.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Transaction } from "src/app/models/transaction";
+
+@Injectable({
+  providedIn: "root",
+})
+export class TransactionService {
+  private url = "https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989";
+  transaction: Transaction = new Transaction();
+  constructor(private http: HttpClient) {}
+
+  formatData(user, valor, card): void {
+    this.transaction.card_number = card.card_number;
+    this.transaction.cvv = card.cvv;
+    this.transaction.expiry_date = card.expiry_date;
+    this.transaction.destination_user_id = user.id;
+    this.transaction.value = valor.value;
+
+    this.sendTransaction(this.transaction);
+  }
+
+  private sendTransaction(transaction) {
+    this.http.post(this.url, transaction).subscribe((res) => {
+      console.log(res);
+    });
+  }
+}
