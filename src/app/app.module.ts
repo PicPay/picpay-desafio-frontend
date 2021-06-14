11d34f93b481407812866776d0c00d578e8351c6
Douglas Carvalho <douglasdorigo2@gmail.com>
2021-06-14 17:31:59 -0300

    Altera app para configurar casca inicial do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NgxLoadingModule } from 'ngx-loading';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BsModalRef, ModalModule } from 'ngx-bootstrap/modal';
+import { CurrencyMaskModule } from 'ngx-currency-mask';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    NgxLoadingModule.forRoot({}),
+    FormsModule,
+    ReactiveFormsModule,
+    ModalModule.forRoot(),
+    CurrencyMaskModule
   ],
-  providers: [],
+  entryComponents: [],
+  providers: [
+    FormBuilder,
+    BsModalRef
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
