eb793437e1fb9f0867b57cfc4c1843a3723f9776
Adm <adm@MacBook-Air-de-Danilo.local>
2021-06-25 21:08:09 -0300

    chore: add jest for unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'picpay-desafio-frontend'`, () => {
+  it(`should have as title 'Desafio Picpay Front-end'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('picpay-desafio-frontend');
+    expect(app.title).toEqual('Desafio Picpay Front-end');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('picpay-desafio-frontend app is running!');
+    expect(compiled.querySelector('.content span')).toContain('picpay-desafio-frontend app is running!');
   });
 });
