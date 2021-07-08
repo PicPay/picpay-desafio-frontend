566e26f6ad42fce80908d3efc70a4084a5b7c6ea
Lucas Nogueira <lucas_snogueira@hotmail.com>
2021-07-08 01:11:32 -0300

    feat(base-header): add header component

--- src/app/base/base.module.ts
+++ src/app/base/base.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BasePageComponent } from './components/base-page/base-page.component';
 import { BaseModalComponent } from './components/base-modal/base-modal.component';
+import { HeaderComponent } from './components/base-page/components/header/header.component';
 import { MaterialBaseModule } from './material-base.module';
 
 const components = [
   BasePageComponent,
   BaseModalComponent,
+  HeaderComponent,
 ];
 
 @NgModule({
