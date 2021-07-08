8c278929efeca9e64c88c999940bfcbd234de8b3
LiaLobo <lia.lobo@gmail.com>
2021-07-07 22:11:21 -0300

    remove feedback modal component imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,13 @@ import { NgModule } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component'
 import { UsersListComponent } from './pages/users-list/users-list.component'
-import { FeedbackModalComponent } from './components/feedback-modal/feedback-modal.component'
 import { PaymentModalComponent } from './components/payment-modal/payment-modal.component'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-
-//import ReactiveFormsModule here
-// import { ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersListComponent,
-    FeedbackModalComponent,
     PaymentModalComponent
   ],
   imports: [
