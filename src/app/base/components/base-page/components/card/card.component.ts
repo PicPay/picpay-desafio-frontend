100f5daab356d63f8a6c5c1d937a034f82940e71
Lucas Nogueira <lucas_snogueira@hotmail.com>
2021-07-09 11:30:14 -0300

    feat(card): card component enhancements

--- src/app/base/components/base-page/components/card/card.component.ts
+++ src/app/base/components/base-page/components/card/card.component.ts
@@ -1,15 +1,42 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  animations: [
+    trigger('cardFlip', [
+      state('default', style({
+        transform: 'none'
+      })),
+      state('flipped', style({
+        transform: 'rotateX(180deg)'
+      })),
+      transition('default => flipped', [
+        animate('400ms')
+      ]),
+      transition('flipped => default', [
+        animate('200ms')
+      ])
+    ])
+  ],
 })
 export class CardComponent implements OnInit {
+
+  state: 'flipped' | 'default' = 'default';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClickCard(): void {
+    if (this.state === 'default') {
+      this.state = 'flipped';
+    } else {
+      this.state = 'default';
+    }
   }
 
 }
