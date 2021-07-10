9567cda8dd08fee9d976129cdcb0cfd5698128f2
pauloricardos <paulorsscontato@hotmail.com>
2021-07-10 16:48:38 -0300

    :heavy_plus_sign: add ng lazy load library responsible to render user's avatar from api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
+import { LazyLoadImageModule } from 'ng-lazyload-image';
+
 import { AppComponent } from './app.component';
 import { UsersComponent } from './pages/users/users.component';
 import { TransactionComponent } from './pages/transaction/transaction.component';
@@ -10,7 +12,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, UsersComponent, TransactionComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule, LazyLoadImageModule],
   providers: [],
   bootstrap: [AppComponent],
 })
