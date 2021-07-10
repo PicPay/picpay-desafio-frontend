255b21783f95ba4764c1381eb4fb2f9eb8ffb3a6
pauloricardos <paulorsscontato@hotmail.com>
2021-07-10 15:46:05 -0300

    :heavy_plus_sign: import necessary modules, components and routeto app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './pages/users/users.component';
+import { PaymentComponent } from './pages/payment/payment.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, UsersComponent, PaymentComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
