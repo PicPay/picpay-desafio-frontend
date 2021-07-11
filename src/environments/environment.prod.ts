bbe59883e3d3367a2377beeda5da7284f838cbab
Lucas Nogueira <lucas_snogueira@hotmail.com>
2021-07-10 22:43:46 -0300

    fix(*): general layout adjustments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url_info_cashback: 'https://card.picpay.com/?gclid=Cj0KCQjwxJqHBhC4ARIsAChq4asf5iNzZegl2ANTq8lUSSl4pZg9QW9FkN_v-1qCV7mHHMkToWzc-4YaAuO1EALw_wcB',
+  url_info_cashback: 'https://picpay.com/site/download?shortlink=df2d0c2e&pid=Site&shortlink=df2d0c2e&pid=Site&c=Bot%C3%A3o%20Download%20-%20Site&af_force_deeplink=true',
   users: {
     getAll: 'https://www.mocky.io/v2/5d531c4f2e0000620081ddce',
   },
