7e7d5e4cc9d075a0d72cfaec6772aa419339d71a
Lucas Nogueira <lucas_snogueira@hotmail.com>
2021-07-12 09:13:57 -0300

    chore:(base-modal): remove unused component

--- src/app/base/base.module.ts
+++ src/app/base/base.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BasePageComponent } from './components/base-page/base-page.component';
-import { BaseModalComponent } from './components/base-modal/base-modal.component';
 import { HeaderComponent } from './components/base-page/components/header/header.component';
 import { MaterialBaseModule } from './modules/material-base.module';
 import { SharedModule } from '../shared/shared.module';
 
 const components = [
   BasePageComponent,
-  BaseModalComponent,
   HeaderComponent,
 ];
 
