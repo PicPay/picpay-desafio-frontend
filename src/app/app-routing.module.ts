e90cf335bb13163f5e72266e073f1ee4a1f74785
pauloricardos <paulorsscontato@hotmail.com>
2021-07-12 15:55:00 -0300

    :sparkles: add transaction route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TransactionComponent } from './pages/transaction/transaction.component';
 import { UsersComponent } from './pages/users/users.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'users', pathMatch: 'full' },
-  { path: 'users', component: UsersComponent }
+  { path: 'users', component: UsersComponent },
+  { path: 'users/:id/transaction', component: TransactionComponent }
 ];
 
 @NgModule({
