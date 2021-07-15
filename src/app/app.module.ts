f951229a89d114ca1a084aa3e0de703b665fa89a
Raquel Sousa <pereiraraquel.sousa@gmail.com>
2021-07-14 21:08:39 -0300

    feat: adding module shared
    adding module shared in project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './Pages/home/home.component';
+import { SharedModule } from './Shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SharedModule
   ],
   imports: [
     BrowserModule,
