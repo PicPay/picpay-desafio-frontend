221e0e3bab8a3a73b05517d325ef16e03ed9cb9b
pauloricardos <paulorsscontato@hotmail.com>
2021-07-15 18:48:18 -0300

    :white_check_mark: add tests to user and transaction services

--- src/app/services/transaction/transaction.service.spec.ts
+++ src/app/services/transaction/transaction.service.spec.ts
@@ -1,16 +1,36 @@
-/* tslint:disable:no-unused-variable */
+import { TestBed, getTestBed } from '@angular/core/testing';
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
 import { TransactionService } from './transaction.service';
 
 describe('Service: Transaction', () => {
+  let injector: TestBed;
+  let service: TransactionService;
+  let httpMock: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [TransactionService]
     });
+    injector = getTestBed();
+    service = injector.get(TransactionService);
+    httpMock = injector.get(HttpTestingController);
   });
 
-  it('should ...', inject([TransactionService], (service: TransactionService) => {
-    expect(service).toBeTruthy();
-  }));
+  it ('#post should return success true', () => {
+    const mockedPayload = {
+      card_number: '1111111111111111',
+      cvv: 789,
+      expiry_date: '01/18',
+      destination_user_id: 1,
+      value: 5
+    };
+
+    service.payUser(mockedPayload).subscribe(result => {
+      expect(result).toContain({ success: true });
+      expect(result).toContain({ status: 'Aprovada' });
+    });
+  });
 });
--- src/app/services/user/user.service.spec.ts
+++ src/app/services/user/user.service.spec.ts
@@ -1,16 +1,43 @@
-/* tslint:disable:no-unused-variable */
+import { TestBed, getTestBed } from '@angular/core/testing';
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
 import { UserService } from './user.service';
 
 describe('Service: User', () => {
+  let injector: TestBed;
+  let service: UserService;
+  let httpMock: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [UserService]
     });
+    injector = getTestBed();
+    service = injector.get(UserService);
+    httpMock = injector.get(HttpTestingController);
   });
 
-  it('should ...', inject([UserService], (service: UserService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('#get should return array of users', () => {
+    const dummyUsers = [
+      {
+        id: 1,
+        name: 'Rodolfo',
+        img: 'https://randomuser.me/api/portraits/men/9.jpg',
+        username: '@rodolfo'
+      },
+      {
+        id: 2,
+        name: 'Julia',
+        img: 'https://randomuser.me/api/portraits/men/9.jpg',
+        username: '@julia'
+      }
+    ];
+
+    service.users.subscribe(users => {
+      expect(users.length).toBe(2);
+      expect(users).toEqual(dummyUsers);
+    });
+  });
 });
