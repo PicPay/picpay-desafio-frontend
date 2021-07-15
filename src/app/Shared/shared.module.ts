3a038a09dae047ab46860509f188d3585aa855b4
Raquel Sousa <pereiraraquel.sousa@gmail.com>
2021-07-14 21:29:32 -0300

    feat: :sparkles: creating new component
    create new component for list

--- src/app/Shared/shared.module.ts
+++ src/app/Shared/shared.module.ts
@@ -4,14 +4,15 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { ButtonComponent } from './button/button.component';
 import { ModalComponent } from './modal/modal.component';
+import { ListComponent } from './list/list.component';
 
 
 
 @NgModule({
-  declarations: [FooterComponent, HeaderComponent, ButtonComponent, ModalComponent],
+  declarations: [FooterComponent, HeaderComponent, ButtonComponent, ModalComponent, ListComponent],
   imports: [
     CommonModule
   ],
-  exports: [FooterComponent, HeaderComponent, ButtonComponent, ModalComponent]
+  exports: [FooterComponent, HeaderComponent, ButtonComponent, ModalComponent, ListComponent]
 })
 export class SharedModule { }
