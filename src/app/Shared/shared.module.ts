31f05f457b931aae51164420cfd5886b8e641cbf
Raquel Sousa <pereiraraquel.sousa@gmail.com>
2021-07-14 21:03:20 -0300

    feat: :tada: scructure for components
    creating scructure for components

--- src/app/Shared/shared.module.ts
+++ src/app/Shared/shared.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { ButtonComponent } from './button/button.component';
+import { ModalComponent } from './modal/modal.component';
 
 
 
 @NgModule({
-  declarations: [FooterComponent, HeaderComponent, ButtonComponent],
+  declarations: [FooterComponent, HeaderComponent, ButtonComponent, ModalComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [FooterComponent, HeaderComponent, ButtonComponent, ModalComponent]
 })
 export class SharedModule { }
