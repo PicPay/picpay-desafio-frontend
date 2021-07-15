1380b90ae5f62aeefa1885eae8173eb77e44618d
Rafaella Carmo Menezes <carmo.rafaella@yahoo.com.br>
2021-07-14 21:56:46 -0300

    Feat: add user service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CardPaymentComponent } from './card-payment/card-payment.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardPaymentService } from './card-payment/card-payment.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardPaymentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CardPaymentService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
